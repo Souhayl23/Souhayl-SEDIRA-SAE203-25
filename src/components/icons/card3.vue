377e9f0f4891c4dff07e590ceacdca0ddd8462df
Souhayl23 <souhayl.sedira23@gmail.com>
2022-06-03 17:25:46 +0200

    commit

--- src/components/icons/card3.vue
+++ src/components/icons/card3.vue
@@ -3,7 +3,7 @@
   class="flex flex-col m-[60px] w-[75%] max-w-[400px] h-auto relative overflow-hidden rounded-xl bg-white"
   style="box-shadow: 0px 2px 4px 0 rgba(28,5,77,0.1), 0px 12px 32px 0 rgba(0,0,0,0.05);"
 >
-  <img src="../../assets/LE FESTIVAL.png" class="h-[195px] self-stretch flex-grow object-cover" />
+  <img src="../../assets/LEFESTIVAL.png" class="h-[195px] self-stretch flex-grow object-cover" />
   <div
     class="flex flex-col self-stretch flex-grow-0 flex-shrink-0 relative gap-1 px-6 py-4 bg-white"
   >
